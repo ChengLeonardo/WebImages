11cdaf33df48b71c309cbc074f3df52914d527f7
ChengLeonardo <leonardo.chenget12de1@gmail.com>
2024-08-15 17:41:25 -0300

    base de datos

---
+++ Backend/BaseDatos/00-DDL.sql
@@ -0,0 +1,52 @@
+-- Crear la base de datos
+CREATE DATABASE IF NOT EXISTS MiPaginaDeImagenes;
+USE MiPaginaDeImagenes;
+
+-- Tabla RolUsuario
+CREATE TABLE RolUsuario (
+    IdRol INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    Descripcion VARCHAR(50) NOT NULL
+);
+
+-- Tabla Usuario
+CREATE TABLE Usuario (
+    IdUsuario INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    Nombre VARCHAR(50) NOT NULL,
+    Apellido VARCHAR(50) NOT NULL,
+    NombreUsuario VARCHAR(255) UNIQUE NOT NULL,
+    Contrasena VARCHAR(255) NOT NULL,
+    Email VARCHAR(100) UNIQUE NOT NULL,
+    FotoPerfil VARCHAR(255), -- URL de la imagen de perfil
+    IdRol INT UNSIGNED,
+    FOREIGN KEY (IdRol) REFERENCES RolUsuario(IdRol)
+);
+
+-- Tabla Post
+CREATE TABLE Post (
+    IdPost INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    UrlImagen VARCHAR(255) NOT NULL, -- URL de la imagen
+    IdUsuario INT UNSIGNED,
+    CantidadLikes INT UNSIGNED DEFAULT 0,
+    FechaPublicacion DATE NOT NULL,
+    FOREIGN KEY (IdUsuario) REFERENCES Usuario(IdUsuario)
+);
+
+-- Tabla UsuarioLikes
+CREATE TABLE UsuarioLikes (
+    IdUsuario INT UNSIGNED,
+    IdPost INT UNSIGNED,
+    FechaLike DATE NOT NULL,
+    PRIMARY KEY (IdUsuario, IdPost),
+    FOREIGN KEY (IdUsuario) REFERENCES Usuario(IdUsuario),
+    FOREIGN KEY (IdPost) REFERENCES Post(IdPost)
+);
+
+-- Tabla Seguidores
+CREATE TABLE Seguidores (
+    IdUsuario INT UNSIGNED,
+    IdUsuarioSeguido INT UNSIGNED,
+    FechaSeguimiento DATE NOT NULL,
+    PRIMARY KEY (IdUsuario, IdUsuarioSeguido),
+    FOREIGN KEY (IdUsuario) REFERENCES Usuario(IdUsuario),
+    FOREIGN KEY (IdUsuarioSeguido) REFERENCES Usuario(IdUsuario)
+);
---
+++ Backend/BaseDatos/01-InsertProcedures.sql
@@ -0,0 +1,62 @@
+USE MiPaginaDeImagenes;
+DELIMITER //
+
+-- Stored Procedure para insertar en la tabla Usuario
+CREATE PROCEDURE InsertUsuario(
+    IN p_Nombre VARCHAR(50),
+    IN p_Apellido VARCHAR(50),
+    IN p_NombreUsuario VARCHAR(255),
+    IN p_Contrasena VARCHAR(255),
+    IN p_Email VARCHAR(100),
+    IN p_FotoPerfil VARCHAR(255),
+    IN p_IdRol INT UNSIGNED
+)
+BEGIN
+    INSERT INTO Usuario (Nombre, Apellido, NombreUsuario, Contrasena, Email, FotoPerfil, IdRol)
+    VALUES (p_Nombre, p_Apellido, p_NombreUsuario, p_Contrasena, p_Email, p_FotoPerfil, p_IdRol);
+END //
+
+-- Stored Procedure para insertar en la tabla Post
+CREATE PROCEDURE InsertPost(
+    IN p_UrlImagen VARCHAR(255),
+    IN p_IdUsuario INT UNSIGNED,
+    IN p_CantidadLikes INT UNSIGNED,
+    IN p_FechaPublicacion DATE
+)
+BEGIN
+    INSERT INTO Post (UrlImagen, IdUsuario, CantidadLikes, FechaPublicacion)
+    VALUES (p_UrlImagen, p_IdUsuario, p_CantidadLikes, p_FechaPublicacion);
+END //
+
+-- Stored Procedure para insertar en la tabla UsuarioLikes
+CREATE PROCEDURE InsertUsuarioLikes(
+    IN p_IdUsuario INT UNSIGNED,
+    IN p_IdPost INT UNSIGNED,
+    IN p_FechaLike DATE
+)
+BEGIN
+    INSERT INTO UsuarioLikes (IdUsuario, IdPost, FechaLike)
+    VALUES (p_IdUsuario, p_IdPost, p_FechaLike);
+END //
+
+-- Stored Procedure para insertar en la tabla Seguidores
+CREATE PROCEDURE InsertSeguidores(
+    IN p_IdUsuario INT UNSIGNED,
+    IN p_IdUsuarioSeguido INT UNSIGNED,
+    IN p_FechaSeguimiento DATE
+)
+BEGIN
+    INSERT INTO Seguidores (IdUsuario, IdUsuarioSeguido, FechaSeguimiento)
+    VALUES (p_IdUsuario, p_IdUsuarioSeguido, p_FechaSeguimiento);
+END //
+
+-- Stored Procedure para insertar en la tabla RolUsuario
+CREATE PROCEDURE InsertRolUsuario(
+    IN p_Descripcion VARCHAR(50)
+)
+BEGIN
+    INSERT INTO RolUsuario (Descripcion)
+    VALUES (p_Descripcion);
+END //
+
+DELIMITER ;
