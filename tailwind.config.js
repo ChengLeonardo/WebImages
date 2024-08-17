800ac413d2f6759106d03b627536d7ed55ad23ee
miguel2433 <miguelitocap12@gmail.com>
2024-08-17 00:45:39 -0300

    Login

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content:["./FrontEnd/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        white: "#ffff",
+        azul: "#5378E0",
+        gris: "#D7D9DE"
+      }
+    },
+  },
+  plugins: [],
+}
