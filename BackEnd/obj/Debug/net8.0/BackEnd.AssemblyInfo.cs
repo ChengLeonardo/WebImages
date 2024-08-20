47b84fd466f1454cde964aa106367ba6a1164107
ChengLeonardo <leonardo.chenget12de1@gmail.com>
2024-08-20 10:13:57 -0300

    fasdf

--- BackEnd/obj/Debug/net8.0/BackEnd.AssemblyInfo.cs
+++ BackEnd/obj/Debug/net8.0/BackEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73a7a8596c16e63edde83a00ba95dd35305d9614")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab158e79e56a3af2318abb4de184b5bfb8ae3349")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
