56bf17646b41f35536dc03625f99c4911f4a68f6
Miguel <miguelitocap12@gmail.com>
2024-08-22 17:48:08 -0300

    Perfil Casi completo

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,14 @@ module.exports = {
   content: ["./FrontEnd/**/*.{html,js}"],
   theme: {
     extend: {
+      height:{
+        '11\/12': '91.666667%'
+      },
       colors:{
         white: "#ffff",
         azul: "#5378E0",
-        gris: "#D7D9DE"
+        gris: "#D7D9DE",
+        grisOscuro: "#cbcdd1"
       }
     },
   },
